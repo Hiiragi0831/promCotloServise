784a4a3a0c5d469cc579663ec7ad928da8cb64e7
glazunovden@yandex.ru <27111993Den>
2022-02-09 19:25:14 +0300

    десктоп готов

--- src/app.js
+++ src/app.js
@@ -2,7 +2,27 @@
 
 // Styles
 import 'styles/_app.scss';
-
 import 'bootstrap';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
 require('scripts/demo');
+
+new Swiper(".right-slider", {
+  modules: [Navigation, Pagination],
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  pagination: {
+    el: ".slider-pagination",
+    type: "fraction",
+    formatFractionCurrent: addZero,
+    formatFractionTotal: addZero
+  },
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+});
+
+function addZero(num){
+  return (num > 9) ? num : '0' + num;
+}
