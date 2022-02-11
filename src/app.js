c208933ce5cefbb6ecd1c5c0a2becbf620a81a6c
glazunovden@yandex.ru <27111993Den>
2022-02-11 15:30:35 +0300

    готово

--- src/app.js
+++ src/app.js
@@ -4,28 +4,50 @@
 import 'styles/_app.scss';
 import 'bootstrap';
 import Swiper, { Navigation, Pagination } from 'swiper';
-import WOW from "wow.js/dist/wow";
-
-require('scripts/demo');
-
-new Swiper(".right-slider", {
-  modules: [Navigation, Pagination],
-  slidesPerView: "auto",
-  spaceBetween: 40,
-  pagination: {
-    el: ".slider-pagination",
-    type: "fraction",
-    formatFractionCurrent: addZero,
-    formatFractionTotal: addZero
-  },
-  navigation: {
-    nextEl: ".slider-button-next",
-    prevEl: ".slider-button-prev",
-  },
-});
 
 function addZero(num){
   return (num > 9) ? num : '0' + num;
 }
+document.addEventListener("DOMContentLoaded", function() {
+  new Swiper(".right-slider", {
+    modules: [Navigation, Pagination],
+    slidesPerView: "auto",
+    spaceBetween: 40,
+    pagination: {
+      el: ".slider-pagination",
+      type: "fraction",
+      formatFractionCurrent: addZero,
+      formatFractionTotal: addZero
+    },
+    navigation: {
+      nextEl: ".slider-button-next",
+      prevEl: ".slider-button-prev",
+    },
+  });
 
-new WOW().init();
+  window.addEventListener('scroll', function() {
+    if (window.screen.width >= 768) {
+      for (let item of document.querySelectorAll('.col-12[name]')) {
+        let link = document.querySelector('a[href="#' + item.getAttribute('name') + '"]');
+
+        if (item.offsetTop - 5 < document.scrollingElement.scrollTop - 200) {
+          document.querySelector('.nav-pc').querySelectorAll('.nav-link').forEach(a => {a.classList.remove('active')});
+          link.classList.add('active')
+        } else {
+          link.classList.remove('active')
+        }
+      }
+    }
+
+    if (window.screen.width >= 1200) {
+      if (document.querySelector('.space').clientHeight / 2 < document.scrollingElement.scrollTop) {
+        document.querySelector('.full').classList.add('hidden');
+        document.querySelector('.half').classList.add('visible');
+      } else {
+        document.querySelector('.full').classList.remove('hidden');
+        document.querySelector('.half').classList.remove('visible');
+      }
+    }
+  });
+
+});
